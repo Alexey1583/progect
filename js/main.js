8c6b48db8cdb01252842308929bab4c9bbc73ef2
Alexey <alexeysem1583@gmail.com>
2022-02-16 21:56:30 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,29 @@
+'use strict'; // обозначет испльзование современной дерективы
+
+// const answers = [];
+
+// answers[0] = prompt('Как ваше имя?', '');
+// answers[1] = prompt('Как ваше имя?', '');
+// answers[2] = prompt('Как ваше имя?', '');
+
+// // document.write(answers); старый вывод в  dom заменяет все содержимое страницы
+// console.log(typeof(answers));
+
+// урок 8 Интерполяция
+
+// const category ='toys';
+
+// console.log(`https://somerl.com/${category}/4`);
+
+// const user = 'Ivan';
+
+// alert(`Приет, ${user}`);
+
+// урок 9 Операторы
+let incr = 10,
+	 decr = 10;
+
+	 incr++; //оператор инкримента
+	 decr--; //оператор декримента
+
+	console.log(incr, decr);
